dfb6301737748d81089fd3ae0684ea6834ebd96c
ha1t <ha1t@users.noreply.github.com>
2012-04-02 11:42:26 +0900

    関数化への布石

--- makelink.sh
+++ makelink.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+# TODO:強制的にresetする仕組みを作る
+
 basedir=$HOME/src/vimrc
 targetdir=$HOME
 
-if [ ! -d $HOME/.vim ]; then
-  ln -s $basedir/vim/ $HOME/.vim
-fi
+function makelink_dir () {
+  if [ ! -d $2 ]; then
+    echo "created:" $2
+    ln -s $1 $2
+  fi
+}
+
+makelink_dir $basedir/vim $HOME/.vim
 
 if [ ! -f $HOME/.vimrc ]; then
   ln -s $basedir/vimrc $HOME/.vimrc
