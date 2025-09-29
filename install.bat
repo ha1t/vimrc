cff3d2f5ae93b9bd374b5d92dca00cb3f25a26b8
halt <ha1t@users.noreply.github.com>
2025-09-29 09:56:46 +0900

    add path check

--- install.bat
+++ install.bat
@@ -1,6 +1,19 @@
 @echo off
 chcp 65001
 
+echo Vim.exeのパスを確認します...
+where vim
+if %errorlevel% neq 0 (
+    echo.
+    echo ❌ Vim.exeのパスは通っていません。
+    echo.
+    call :CreateSymlink "CorvusSKK\userdict.txt" "%USERPROFILE%\bin\vim.exe"
+) else (
+    echo.
+    echo ✅ Vim.exeのパスは正常に通っています。
+)
+echo.
+
 mklink /D %USERPROFILE%\.vim %USERPROFILE%\Documents\GitHub\vimrc\vim
 IF EXIST "%USERPROFILE%\.vim" (
     mkdir "%USERPROFILE%\.vim\backup"
