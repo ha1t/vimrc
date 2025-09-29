9a0d7668aef86c71ffe9bf7b1cc785d2f80255e1
halt <ha1t@users.noreply.github.com>
2025-09-29 09:37:23 +0900

    とりあえず設定を簡単に編集できるように

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@
 vim.keymap.set({'n', 'v', 's', 'o'}, ';', ':', { noremap = true })
 vim.keymap.set({'n', 'v', 's', 'o'}, ':', ';', { noremap = true })
 
-vim.api.nvim_create_user_command('Restart', 'qa | nvim', {})
+vim.api.nvim_create_user_command('EditVimrc', 'edit $MYVIMRC', {})
+vim.api.nvim_create_user_command('ReloadVimrc', 'source $MYVIMRC', {})
 
